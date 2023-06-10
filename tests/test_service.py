33117fb00571beaef5849d1c88d10302fa223b4c
Alexander Malev <yttrium@somedev.ru>
2023-06-10 19:03:40 +0300

    Test multiproc mode

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,17 +1,23 @@
+import os
+import tempfile
+
 import pytest
 from aiohttp import client
 
+import aioworkers_prometheus.service
+
 
 @pytest.fixture
-def config_yaml():
-    return """
+def config_yaml(unused_tcp_port_factory):
+    port = unused_tcp_port_factory()
+    return f"""
     prometheus:
         cls: aioworkers_prometheus.service.Service
         labels:
             env: prod
-        port: 8000
+        port: {port}
         graphite:
-            address: localhost:8000
+            address: localhost:{port}
     """
 
 
@@ -23,3 +29,16 @@ async def test_port(context):
             assert r.status == 200, await r.text()
             data = await r.read()
     assert data
+
+
+@pytest.fixture
+def tmp_dir(mocker):
+    env = "PROMETHEUS_MULTIPROC_DIR"
+    with tempfile.TemporaryDirectory() as d:
+        mocker.patch.object(os, "environ", {env: d})
+        mocker.patch.object(aioworkers_prometheus.service, "MULTIPROC_DIR", d)
+        yield d
+
+
+async def test_multiproc_generate_latest(tmp_dir, context):
+    await context.prometheus.generate_latest()
